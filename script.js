4ee7d5df9d40b396efa79fdf9809f955b19a8248
donuts-and <89621960+donuts-and@users.noreply.github.com>
2022-07-21 11:58:18 -0500

    Add files from codepen

---
+++ script.js
@@ -0,0 +1,49 @@
+tippy(".combo-enter", {
+  content: "DH combo: enter" });
+
+
+tippy(".combo-minus", {
+  content: ",. combo: -" });
+
+
+tippy(".combo-underscore", {
+  content: "H/ combo: _",
+  placement: "bottom" });
+
+
+tippy(".combo-copy-c", {
+  content: "VD combo: copy",
+  placement: "right" });
+
+
+tippy(".combo-copy-d", {
+  content: "VD combo: copy",
+  placement: "left" });
+
+
+tippy(".combo-cut-x", {
+  content: "JV combo: cut",
+  placement: "right" });
+
+
+tippy(".combo-cut-c", {
+  content: "JV combo: cut",
+  placement: "left" });
+
+
+tippy(".combo-paste", {
+  content: "JVD combo: paste",
+  placement: "bottom" });
+
+
+tippy(".combo-undo", {
+  content: "QJ combo: undo" });
+
+
+tippy(".combo-ques", {
+  content: "PF combo: ?" });
+
+
+tippy(".sticky", {
+  content: "sticky",
+  placement: "bottom" });
